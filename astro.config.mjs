049b452cd097e6d033043cab05998fae5d2c5f2e
Trevor Rounce <trevor@switchbackcreative.ca>
2026-08-25 10:45:32 -0600

    files setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,22 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  build: {
+    inlineStylesheets: "always",
+  },
+  vite: {
+    css: {
+      devSourcemap: true, // ✅ Enables CSS/SCSS source maps for better debugging
+    },
+  },
+  site: "https://rk-group.ca/",
+  integrations: [react(), sitemap()],
+  image: {
+    domains: ["rk-group.swbdatabases3.ca/"],
+    remotePatterns: [{ protocol: "https" }],
+  },
+});
