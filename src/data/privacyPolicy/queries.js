bd94fa75bdfb0808ce4d06a7cd3040d8eb809df2
Trevor Rounce <trevor@switchbackcreative.ca>
2026-08-26 16:23:25 -0600

    service post page setup

--- src/data/privacyPolicy/queries.js
+++ src/data/privacyPolicy/queries.js
@@ -1,20 +1,3 @@
-export const Hero_Intro_Query = `
-   	 heroIntro: heroIntroComponent {
-        title
-        content
-        image {
-            node {
-                sourceUrl
-                altText
-                mediaDetails {
-                    width
-                    height
-                }
-            }
-        }
-    }
-`;
-
 export const Main_Content_Query = `
     mainContent: mainContentComponent {
         title
