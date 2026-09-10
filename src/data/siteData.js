92852765f924ee5d7a60770030892d486edf2090
Trevor Rounce <trevor@switchbackcreative.ca>
2026-09-09 19:54:53 -0600

    domiain

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -1,11 +1,12 @@
 import defaultSrc from "../../public/meta-default-image.jpg";
 
 export const baseData = {
-  title: "RK Group Engineered | Pipeline Solutions",
-  organizationName: "RK Group Engineered | Pipeline Solutions",
-  description: "RK Group Engineered | Pipeline Solutions",
-  siteImg: `https://rk-group-new.netlify.app/${defaultSrc.src}`,
+  title: "RK Group | Engineered Pipeline Cleaning Solutions",
+  organizationName: "RK Group",
+  description:
+    "RK Group delivers scalable pipeline cleaning solutions that support safe operations, inspection readiness, long-term asset integrity, and quality-driven performance. From fit-for-purpose tools and engineered programs to field execution and expanding capabilities, we help operators achieve reliable, consistent outcomes across the lifecycle of their pipeline assets.",
+  siteImg: `https://rk-group.ca/${defaultSrc.src}`,
   logoUrl: "",
-  url: "https://rk-group-new.netlify.app/",
+  url: "https://rk-group.ca/",
   socialProfiles: [],
 };
